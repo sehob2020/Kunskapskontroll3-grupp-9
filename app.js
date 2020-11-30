320b7cf95c284b43414c32d0e877782abd3dee24
sehob2020 <70333786+sehob2020@users.noreply.github.com>
2020-11-30 14:28:20 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+// https://live.staticflickr.com/7372/12502775644_284b3183ca0a2ee9_w.jpg
+
+function Flickr(){
+    fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=33701ee2717d608a9811f5c5556d9078&gallery_id=72157716271339617&format=rest')
+}
+    
+    
+
+    //console.log(Flickr);
+
+Flickr();
+
+
+    
