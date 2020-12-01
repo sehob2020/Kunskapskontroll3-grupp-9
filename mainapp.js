3ea93ccd1c7bc76a50109412f0d98a2d0e813764
cuong1979 <70330972+cuong1979@users.noreply.github.com>
2020-12-01 16:59:41 +0100

    card bra

--- mainapp.js
+++ mainapp.js
@@ -1,5 +1,6 @@
 const KEY = 'd47e7c95656f6a1961da9190c903ddb2';
 let galleryId = '72157717114209588';
+let cardCount = 0;
 
 // const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&gallery_id=${galleryId}&format=json&nojsoncallback=1`;
 const url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${KEY}&gallery_id=${galleryId}&format=json&nojsoncallback=1`;
@@ -14,6 +15,7 @@ fetch(url).then(
         for(let i=0; i<12; i++){
             console.log(data.photos.photo[i]);
             getImageUrl(data.photos.photo[i]);
+            cardCount++
         }
     }
 )
@@ -32,10 +34,9 @@ function getImageUrl(photoObject){
 //för att visa bilden
 function displayImg(url){
     let kort = document.querySelector('.card-container');
-
-    for(let i=0; i<25; i++){
+    for(let i=0; i<2; i++){
     let div = document.createElement('div');
-    div.setAttribute('class', 'card '+'card'+[i])
+    div.setAttribute('class', 'card' + 'card' + cardCount)
     kort.appendChild(div);
     let img = document.createElement('img');
     img.src = url;
