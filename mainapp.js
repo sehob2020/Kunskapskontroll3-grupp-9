6c22ea4a58303d44b616f0e791115871d7401e10
Simon.Brandone <simon.brandone@utb.ecutbildning.se>
2020-12-01 14:37:54 +0100

    Update mainapp.js

--- mainapp.js
+++ mainapp.js
@@ -1,33 +1,38 @@
-const KEY = 'apikey';
-let searchText = 'soda';
+const KEY = '';
+let searchText = 'animal';
 
-const url = 'URL'
+const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchText}&format=json&nojsoncallback=1&per_page=1&page=10`;
 
 fetch(url).then(
     function(response){
-    return response.json();
+        console.log(response);
+        return response.json();
     }
 ).then(
-    functin(data){
-        console.log(data.photo.photo[0]);
+    function(data){
+        console.log(data.photos.photo[0]);
         getImageUrl(data.photos.photo[0]);
     }
 )
 
+//här ska vi pussla ihop bild urlen
 function getImageUrl(photoObject){
     let photo = photoObject;
-    let size = 'q';
+    let size = 'b';
 
-    let imgUrl = 'https://*****'
+    let imgUrl = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
 
     console.log(imgUrl);
     displayImg(imgUrl);
 }
 
+//för att visa bilden
 function displayImg(url){
     let img = document.createElement('img');
-    img.scr = url;
+    img.src = url;
+    // img.setAttribute('src', url);
 
-    let body = document.querySelector()
-    body.appendChild('img')
+    let body = document.querySelector('body');
+    body.appendChild(img);
+
 }
