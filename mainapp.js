2b7e4089118c2dedb2e9d1783d275ebde5dcfb7a
Simon.Brandone <simon.brandone@utb.ecutbildning.se>
2020-12-03 16:03:49 +0100

    12/03

--- mainapp.js
+++ mainapp.js
@@ -3,6 +3,7 @@ let galleryId = '72157717114209588';
 let cardCount = 0;
 let button = document.querySelector('button')
 let pointsDisplay = document.querySelector('.points')
+pointsDisplay.textContent = 0;
 let kort = document.querySelector('.card-container')
 let cardsChosen = []
 let cardsChosenId = []
@@ -44,33 +45,39 @@ function getImageUrl(photoObject){
 
 function shuffleBoard(max) {
     return Math.floor(Math.random() * Math.floor(max));
-    }
+}
 
 
 //för att visa bilden
 function displayImg(url){
-    let kort = document.querySelector('.card-container');
+    // let kort = document.querySelector('.card-container');
     for(let i=0; i<2; i++){
     let div = document.createElement('div');
-    div.setAttribute('class', 'card ' + 'card-id' + cardCount);
+    // div.setAttribute('class', 'card ' + 'card-id' + cardCount);
+    div.classList.add('card');
+    div.id = cardCount
+    div.dataset.id = cardCount;
     // kort.appendChild(div);
-    var children = kort.childNodes;
+    var children = grid.childNodes;
     var randChild = children[shuffleBoard(children.length)];
-    kort.insertBefore(div, randChild)
+    grid.insertBefore(div, randChild)
+    grid.dataset.id = cardCount;
+    let img1 = document.createElement('img');
+    img1.setAttribute('class', 'front')
+    // div.addEventListener('click', function(event) { turnCard(event.target);})
+    img1.setAttribute('src', '/baksida.png')
+    div.appendChild(img1);
     let img = document.createElement('img');
-    img.setAttribute('class', 'ost')
-    div.addEventListener('click', function(event) { turnCard(event.target);})
-    img.setAttribute('src', '/baksida.png')
-    div.appendChild(img);
-    img = document.createElement('img');
-    img.setAttribute('class', 'ost' + cardCount)
-    img.setAttribute('src', '/baksida.png')
+    img.setAttribute('class', 'back' + cardCount)
+    img.dataset.id = cardCount;
     img.src = url;
     div.appendChild(img);
-    img.style.display = 'none'
+    // img.style.display = 'none'
     bilder.push(img);
     // img.addEventListener('click', turnCard)
     console.log(randChild);
+    console.log(img.id)
+    
     }
 }
 
@@ -80,18 +87,71 @@ function newGame(){
 }
 
 function scoreCount(){
-    pointsDisplay.textContent = 'Congratulations! You found them all!';
-    // alert('hej')
-    console.log(cardNr);
-
+    pointsDisplay.textContent ++
 }
 
-function turnCard(element){
-    //let img = document.querySelector('.baksida');
-    element.style.display = 'none';
-    let ost = document.querySelector('.ost');
-    
-    // bilder.setAttribute('src', '/Card.png')
-    // console.log(bilder)
-    // console.log(bilder)
-}
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+let cards = document.querySelectorAll('.card')
+let grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+kort.appendChild(grid);
+
+const match = () => {
+    const selected = document.querySelectorAll('.selected');
+    selected.forEach(card => {
+      card.classList.add('match');
+    });
+  };
+  
+const resetGuesses = () => {
+    firstGuess = '';
+    secondGuess = '';
+    count = 0;
+    previousTarget = null;
+  
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach(card => {
+    card.classList.remove('selected');
+    });
+};
+
+  grid.addEventListener('click', event => {
+
+    const clicked = event.target;
+  
+    if (
+      clicked.nodeName === 'SECTION' ||
+      clicked === previousTarget ||
+      clicked.parentNode.classList.contains('selected') ||
+      clicked.parentNode.classList.contains('match')
+    ) {
+      return;
+    }
+  
+    if (count < 2) {
+      count++;
+      if (count === 1) {
+        firstGuess = clicked.parentNode.dataset.id;
+        console.log(firstGuess);
+        clicked.parentNode.classList.add('selected');
+      } else {
+        secondGuess = clicked.parentNode.dataset.id;
+        console.log(secondGuess);
+        clicked.parentNode.classList.add('selected');
+      }
+  
+      if (firstGuess && secondGuess) {
+        if (firstGuess === secondGuess) {
+            scoreCount();
+          setTimeout(match, delay);
+        }
+        setTimeout(resetGuesses, delay);
+      }
+      previousTarget = clicked;
+    }
+  
+  });
