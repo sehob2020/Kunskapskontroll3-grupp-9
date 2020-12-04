ccd1d84d33819ad9bf5faf25667e6f00b7c3e2a9
cuong1979 <70330972+cuong1979@users.noreply.github.com>
2020-12-04 14:04:54 +0100

    Update mainapp.js

--- mainapp.js
+++ mainapp.js
@@ -1,3 +1,6 @@
+//Memory Game 9000
+
+// här deklarera vi våra variabler och skapa vår URL till API med hjälps av template literals.
 const KEY = 'd47e7c95656f6a1961da9190c903ddb2';
 let galleryId = '72157717114209588';
 let cardCount = 0;
@@ -5,21 +8,22 @@ let button = document.querySelector('button')
 let pointsDisplay = document.querySelector('.points')
 pointsDisplay.textContent = 0;
 let kort = document.querySelector('.card-container')
-let bilder = [];
+let bilder = []; //skapa en array för att pusha upp alla bilder 
 
+// Bas URL för att hämta bilder i json format.
 const url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${KEY}&gallery_id=${galleryId}&format=json&nojsoncallback=1`;
 
 
 
-
+//fetch för att anropa API .
 fetch(url).then(
     function(response){
         console.log(response);
         return response.json();
     }
 ).then(
-    function(data){
-        for(let i=0; i<12; i++){
+    function(data){ // funktion för att loopa genom bilder och hämta 
+        for(let i=0; i<12; i++){ // for loop för att hämta 12 unika bilder.
             getImageUrl(data.photos.photo[i]);
             cardCount++
         }
@@ -37,22 +41,23 @@ function getImageUrl(photoObject){
 }
 
 
-
+// funktion för random så kortet inte lägger på samma plats 
 function shuffleBoard(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 
-//för att visa bilden
+//för att visa bilden 
+// skapa två bilder bak och fram-sidan
 function displayImg(url){
-    for(let i=0; i<2; i++){
+    for(let i=0; i<2; i++){ // loop för att skaffa dubbletter av bilderna.
     let div = document.createElement('div');
     div.classList.add('card');
     div.id = cardCount
     div.dataset.id = cardCount;
-    var children = grid.childNodes;
-    var randChild = children[shuffleBoard(children.length)];
-    grid.insertBefore(div, randChild)
+    var children = grid.childNodes;// childNodes för grid
+    var randChild = children[shuffleBoard(children.length)];// blanda korten
+    grid.insertBefore(div, randChild)// sättta in 
     grid.dataset.id = cardCount;
     let img1 = document.createElement('img');
     img1.setAttribute('class', 'front')
@@ -63,7 +68,7 @@ function displayImg(url){
     img.dataset.id = cardCount;
     img.src = url;
     div.appendChild(img);
-    bilder.push(img);
+    bilder.push(img);//pusha upp till bilder array
     }
 }
 
@@ -78,8 +83,9 @@ function scoreCount(){
     pointsDisplay.textContent ++
 }
 
-let firstGuess = '';
-let secondGuess = '';
+// variabler för att hålla koll på de olika gissingar
+let firstGuess = '';// första gissing
+let secondGuess = '';// andra gissing
 let count = 0;
 let previousTarget = null;
 let delay = 1200;
@@ -88,6 +94,8 @@ let grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 kort.appendChild(grid);
 
+
+// match funktionen om kort är lika så lägger det till classen match
 const match = () => {
     const selected = document.querySelectorAll('.selected');
     selected.forEach(card => {
@@ -95,6 +103,7 @@ const match = () => {
     });
   };
   
+  // funcktionen gör så att när man clicka på korten så vänds de tillbaka om inte kort matcha.
 const resetGuesses = () => {
     firstGuess = '';
     secondGuess = '';
@@ -114,16 +123,18 @@ const resetTurned = () => {
 
   var selected = document.querySelectorAll('.turned');
   selected.forEach(card => {
-    if(!card.classList.contains('match')){
-      card.classList.remove('turned');
+    if(!card.classList.contains('match')){// två kort som har match stanna 
+      card.classList.remove('turned');// inte två är lika som vänder kort tillbaka 
     }
   });
 };
 
+//en click event för kort
 grid.addEventListener('click', event => {
 
     const clicked = event.target;
   
+    // om clicka på korten sök den efter selected eller match
     if (
       clicked.nodeName === 'SECTION' ||
       clicked === previousTarget ||
@@ -132,7 +143,8 @@ grid.addEventListener('click', event => {
     ) {
       return;
     }
-  
+
+    // om första gissningen och andra gissningen inte matchar går de till turned som kommer vända tillbaka korten , men om de matcha så stannar korten kvar.
     if (count < 2) {
       count++;
       if (count === 1) {
